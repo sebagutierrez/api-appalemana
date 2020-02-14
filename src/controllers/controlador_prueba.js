1e08025811af99f4288581d6c8ca32c0251609fa
Sebastián Gutiérrez <sebantbd@gmail.com>
2020-02-14 15:13:50 -0300

    Se realizan pequeños fixes en el docker-compose.yml. Función list funcionando correctamente y listando todas las tablas.

--- src/controllers/controlador_prueba.js
+++ src/controllers/controlador_prueba.js
@@ -2,22 +2,23 @@ const CasHiba = require('../models').CasHiba;
 const HibaSnomed = require('../models').HibaSnomed;
 const Transitiva = require('../models').Transitiva;
 
-/*
-const conceptosIds = await CasHiba.findAll({
-    limit: 50,
-});
-*/
-
-
 module.exports = {
     async list(req, res) {
         try {
             const cas_hiba_table = await CasHiba.findAll({
                 attributes: ['concepto_id', 'description_nombre', 'description_id', 'concept_id_HIBA', 'tipo_termino', 'termino_preferido'],
-                limit: 10
+                limit: 20
+            });
+            const hiba_snomed_table = await HibaSnomed.findAll({
+                attributes: ['description_id', 'conceptid_HIBA', 'description_preferido_id', 'description_preferido', 'conceptidSN', 'descripcionConceptoSN'],
+                limit:20
+            });
+            const transitiva_table = await Transitiva.findAll({
+                attributes: ['hijo', 'padre', 'es_directo'],
+                limit:20
             });
             res.json({
-                data: cas_hiba_table
+                data: [cas_hiba_table, hiba_snomed_table, transitiva_table]
             })
         } catch (error) {
             res.json({
@@ -27,5 +28,3 @@ module.exports = {
         };
     }   
 }
-
-
--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,14 +1,3 @@
-/*
-module.exports = {
-  index: (req, res) => {
-    return res.status(200).json({
-      message: `API Boilerplate ${process.env.API_VERSION}`,
-      currentLang: 'es_CL',
-    });
-  },
-};
-*/
-
 const controlador_prueba = require('./controlador_prueba');
 
 module.exports = {
